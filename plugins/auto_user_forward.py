c158a074ed9f68706e49a61965eac4c009421d47
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-06-15 20:16:06 +0530

    he

---
+++ plugins/auto_user_forward.py
@@ -0,0 +1,66 @@
+import asyncio, re
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from vars import FROM_DB, TO_DB
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+media_filter = filters.document | filters.video
+lock = asyncio.Lock()
+
+forwarded = 0
+count = 3593
+mcount = 991
+
+@Client.on_message(filters.chat(FROM_DB) & media_filter)
+async def auto_forward(bot, message):
+    global forwarded, count, mcount
+    file_caption = re.sub(r"(❤️‍🔥 Join ~ [ @Moonknight_media ])|(🤖 Join Us \[@BoB_Files1\])|(𝗧𝗛𝗘 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥 )|(𝙿𝚘𝚠𝚎𝚛e𝚍 𝙱𝚢 ➥  @Theprofessers)|(❤️‍🔥 Join ~ [ @Moonknight_media ])|(\n🔸 Upload By \[@BlackDeath_0\])|(\n❤️‍🔥 Join ~ \[@Moonknight_media\])|(@Ac_Linkzz)|(\n⚡️Join:- \[@BlackDeath_0\]‌‌)|(EonMovies)|(\nJOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+)", " ", str(message.caption))
+    file_caption = f"""<b>{file_caption}</b>\n➖➖➖➖➖➖➖➖➖➖
+ <b>Powered By:</b> <a href='https://t.me/MovieTimesTV'><b>Mᴏᴠɪᴇ Tɪᴍᴇs™</b></a>
+➖➖➖➖➖➖➖➖➖➖"""
+    async with lock:
+        try:
+            if count != 0:
+                if mcount != 0:
+                    await message.copy(
+                            chat_id=int(TO_DB),
+                            caption=file_caption
+                        )
+                    forwarded += 1
+                    count -= 1
+                    mcount -= 1
+                    logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TO_DB}\nforwarded {forwarded} files")
+                    await asyncio.sleep(2)
+                else:
+                    logger.info(f"⏸️ {forwarded} files sent! Taking a break of 10 minutes...")
+                    await asyncio.sleep(600)
+                    mcount = 991
+            else:
+                logger.info(f"⏸️ {forwarded} files sent! Taking a break of 1 hour...")
+                await asyncio.sleep(3600)
+                count = 3593
+        except FloodWait as e:
+            logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
+            await asyncio.sleep(e.value + 2)
+            if count != 0:
+                if mcount != 0:
+                    await message.copy(
+                            chat_id=int(TO_DB),
+                            caption=file_caption
+                        )
+                    forwarded += 1
+                    count -= 1
+                    mcount -= 1
+                    logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TO_DB}\nforwarded {forwarded} files")
+                    await asyncio.sleep(2)
+                else:
+                    logger.info(f"⏸️ {forwarded} files sent! Taking a break of 10 minutes...")
+                    await asyncio.sleep(600)
+                    mcount = 991
+            else:
+                logger.info(f"⏸️ {forwarded} files sent! Taking a break of 1 hour...")
+                await asyncio.sleep(3600)
+                count = 3593
---
+++ vars.py
@@ -0,0 +1,25 @@
+from os import environ
+import re
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+SESSION = environ.get("SESSION", "N2 forward bot")
+USER_SESSION = environ.get("USER_SESSION", "") #Pyrogram session dalna hai yahan
+API_ID = 25163484
+API_HASH = "145bcbc424d1c1ffe04f3e607ea55c9a"
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002137528664"))
+PORT = int(environ.get("PORT", "8080"))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6302921275').split()]
+TARGET_DB = int(environ.get("TARGET_DB", "-1002155536121"))
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/MandaliWala/File-Forward-Bottermux")
+#Auto Forward vars
+FROM_DB = [int(channel_id) for channel_id in environ.get('FROM_DB', '-1002248826741 -1002116767957 -1002464939396 -1002753463498 -1001971879597 -1002510094945 -1002056617893').split() if re.match(r'^-?\d+$', channel_id)]
+TO_DB = int(environ.get("TO_DB", "-1002224925300"))
